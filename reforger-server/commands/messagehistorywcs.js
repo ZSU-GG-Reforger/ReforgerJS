432306e1cf7cd55eaa2dc449cbc8e0d289c98a35
Martinez <logan.m269@gmail.com>
2025-05-24 11:01:30 +1000

    WCS commands to interact with logging database

---
+++ reforger-server/commands/messagehistorywcs.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('messagehistorywcs')
+        .setDescription('Retrieve player chat message history from WCS by UUID or name')
+        .addStringOption(option =>
+            option
+                .setName('identifier')
+                .setDescription('The UUID or UserName of the player')
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
+            option
+                .setName('server')
+                .setDescription('Server Number (leave empty for all servers)')
+                .setRequired(false)
+        )
+};
